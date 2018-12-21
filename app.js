fbc13522a84c2be06c47d3a8410a4b7a7fb972fd
adrianln <aleonhost@gmail.com>
2018-12-21 13:36:06 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,39 @@
+// Requires importacion de librerias de 3eros o personalizadas 
+
+var express = require('express');
+var mongoose = require('mongoose');
+
+// Inicializar variables donde vamos a utilizar la libreria
+
+
+var app = express();
+
+//conexion a la base de datos
+
+
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos: \x1b[32m%s\x1b[0m ', ' online');
+});
+
+//Rutas
+
+app.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'peticion realizada correctamente'
+    });
+
+});
+
+
+
+// escuchar peticiones
+
+
+app.listen(3000, () => {
+    console.log('Express server corriendo en el puerto 3000: \x1b[32m%s\x1b[0m ', ' online')
+});
